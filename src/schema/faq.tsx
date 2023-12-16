91c372a304017e0b6762331954f7ddaaf30790fc
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2023-12-16 13:14:36 +0600

    Project Setup Completed

--- src/schema/faq.tsx
+++
@@ -1,6 +0,0 @@
-import * as yup from "yup";
-
-export const faqSchema = yup.object().shape({
-  question: yup.string().required("Question is required").min(2).max(500),
-  answer: yup.string().required("Answer is required").min(2).max(1000),
-});

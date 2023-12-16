72e92ae7e3ca16a0b8254418de637da761758830
mahiuddinhabib <mh.habib137@gmail.com>
2023-12-17 03:12:45 +0600

    video section: basic structure created

---
+++ src/components/MeatOurTeam.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MeatOurTeam = () => {
+    return (
+        <div>
+            <h1>Meat Our Team</h1>
+        </div>
+    );
+};
+
+export default MeatOurTeam;

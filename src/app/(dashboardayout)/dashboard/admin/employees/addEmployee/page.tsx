134740c78e561c2877be1f75dd0ac4cb644f0f8b
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2023-12-22 16:22:37 +0600

    [DONE] add employees stepper form

--- src/app/(dashboardayout)/dashboard/admin/employees/addEmployee/page.tsx
+++ src/app/(dashboardayout)/dashboard/admin/employees/addEmployee/page.tsx
@@ -1,9 +1,48 @@
+"use client";
+import EmployeesFormOne from "@/components/EmployeesForm/EmployeesFormOne";
+import EmployeesFormThree from "@/components/EmployeesForm/EmployeesFormThree";
+import EmployeesFormTwo from "@/components/EmployeesForm/EmployeesFormTwo";
+import StepperForm from "@/components/StepperForm/StepperForm";
+
+const steps = [
+  {
+    title: "Employees Info 1",
+    content: <EmployeesFormOne />,
+  },
+  {
+    title: "Employees Info 2",
+    content: <EmployeesFormTwo />,
+  },
+  {
+    title: "Employees Info 3",
+    content: <EmployeesFormThree />,
+  },
+];
+
+const handleEmployeesSubmit = async (values: any) => {
+  try {
+    console.log(values);
+  } catch (err: any) {
+    console.error(err.message);
+  }
+};
+
 const AddEmployee = () => {
-    return (
-        <div>
-            <h1>AddEmployee</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-3xl font-extrabold text-center my-8">
+        Add Employees
+      </h1>
+      <StepperForm
+        persistKey="addEmployeesForm"
+        navigateLink="/dashboard/admin/employees/addEmployee"
+        submitHandler={(value) => {
+          handleEmployeesSubmit(value);
+        }}
+        steps={steps}
+      />
+    </div>
+  );
 };
 
 export default AddEmployee;
---
+++ src/components/StepperForm/StepperForm.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import {
+  getFromLocalStorage,
+  removeFromLocalStorage,
+  setToLocalStorage,
+} from "@/utils/localStorage";
+import { Button, ConfigProvider, message, Steps } from "antd";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+
+interface ISteps {
+  title?: string;
+  content?: React.ReactElement | React.ReactNode;
+}
+
+interface IStepsProps {
+  steps: ISteps[];
+  persistKey: string;
+  submitHandler: (el: any) => void;
+  navigateLink?: string;
+}
+
+const StepperForm = ({
+  steps,
+  submitHandler,
+  navigateLink,
+  persistKey,
+}: IStepsProps) => {
+  const router = useRouter();
+
+  const [current, setCurrent] = useState<number>(
+    !!getFromLocalStorage("step")
+      ? Number(JSON.parse(getFromLocalStorage("step") as string).step)
+      : 0
+  );
+
+  const [savedValues, setSavedValues] = useState(
+    !!getFromLocalStorage(persistKey)
+      ? JSON.parse(getFromLocalStorage(persistKey) as string)
+      : ""
+  );
+
+  useEffect(() => {
+    setToLocalStorage("step", JSON.stringify({ step: current }));
+  }, [current]);
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+
+  const items = steps.map((item) => ({ key: item.title, title: item.title }));
+
+  const methods = useForm({ defaultValues: savedValues });
+  const watch = methods.watch();
+
+  useEffect(() => {
+    setToLocalStorage(persistKey, JSON.stringify(watch));
+  }, [watch, persistKey, methods]);
+
+  const { handleSubmit, reset } = methods;
+
+  const handleStudentOnSubmit = (data: any) => {
+    submitHandler(data);
+    reset();
+    removeFromLocalStorage("step");
+    removeFromLocalStorage(persistKey);
+    navigateLink && router.push(navigateLink);
+  };
+
+  return (
+    <>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#00674A",
+          },
+        }}
+      >
+        <Steps current={current} items={items} />
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(handleStudentOnSubmit)}>
+            <div>{steps[current].content}</div>
+            <div style={{ marginTop: 24 }}>
+              {current > 0 && (
+                <Button className="mr-5" onClick={() => prev()}>
+                  <span>
+                    <LeftOutlined /> Previous
+                  </span>
+                </Button>
+              )}
+
+              {current < steps.length - 1 && (
+                <Button onClick={() => next()}>
+                  <span>
+                    Next <RightOutlined />
+                  </span>
+                </Button>
+              )}
+              {current === steps.length - 1 && (
+                <Button
+                  type="primary"
+                  className="bg-[#00674A]"
+                  htmlType="submit"
+                  onClick={() => message.success("Processing complete!")}
+                >
+                  Done
+                </Button>
+              )}
+            </div>
+          </form>
+        </FormProvider>
+      </ConfigProvider>
+    </>
+  );
+};
+
+export default StepperForm;

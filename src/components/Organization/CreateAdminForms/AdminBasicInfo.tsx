aef500df0401901baeea7c31a2ed448ac8f28423
mahiuddinhabib <mh.habib137@gmail.com>
2023-12-27 18:51:10 +0600

    create admin done

--- src/components/Organization/CreateAdminForms/AdminBasicInfo.tsx
+++ src/components/Organization/CreateAdminForms/AdminBasicInfo.tsx
@@ -1,19 +1,19 @@
 import { Col, Row } from "antd";
 import {
-  countryOptions,
-  employmentStatus,
   genderOptions,
 } from "@/constants/global";
 import FormInput from "@/components/Forms/FormInput";
 import NormalDatePicker from "@/components/Forms/NormalDatePicker";
 import FormSelectField from "@/components/Forms/FormSelectField";
+import UploadImage from "@/components/ui/UploadImage";
 
 const AdminBasicInfoForm = () => {
   return (
     <>
       <div className="border border-gray-300 rounded-md p-4 my-4">
+        <UploadImage name="profile_picture" />
         <Row gutter={{ xs: 4, md: 20 }}>
-          <Col xs={24} md={12} lg={8} className="mt-3">
+          <Col xs={24} md={12} lg={6} className="mt-3">
             <FormInput
               name="first_name"
               placeholder="Enter First Name"
@@ -21,17 +21,7 @@ const AdminBasicInfoForm = () => {
               size="large"
             />
           </Col>
-
-          <Col xs={24} md={12} lg={8} className="mt-3">
-            <FormInput
-              name="middle_name"
-              placeholder="Enter Middle Name"
-              label="Enter Middle Name"
-              size="large"
-            />
-          </Col>
-
-          <Col xs={24} md={12} lg={8} className="mt-3">
+          <Col xs={24} md={12} lg={6} className="mt-3">
             <FormInput
               name="last_name"
               placeholder="Enter Last Name"
@@ -39,16 +29,14 @@ const AdminBasicInfoForm = () => {
               size="large"
             />
           </Col>
-
-          <Col xs={24} md={12} lg={12} className="mt-3">
+          <Col xs={24} md={12} lg={6} className="mt-3">
             <NormalDatePicker
               name="date_of_birth"
               label="Date of birth"
               size="large"
             />
           </Col>
-
-          <Col xs={24} md={12} lg={12} className="mt-3">
+          <Col xs={24} md={12} lg={6} className="mt-3">
             <FormSelectField
               size="large"
               name="gender"
@@ -57,24 +45,23 @@ const AdminBasicInfoForm = () => {
               placeholder="Select"
             />
           </Col>
-          <Col xs={24} md={12} lg={12} className="mt-3">
-            <FormSelectField
-              size="large"
-              name="employment_status"
-              options={employmentStatus}
-              label="Employee Status"
-              placeholder="Select"
-            />
-          </Col>
-          <Col xs={24} md={12} lg={12} className="mt-3">
+          <Col xs={24} md={12} lg={6} className="mt-3">
             <FormInput
-              name="employee_code"
-              placeholder="Enter Employee code"
-              label="Employee Code"
+              name="phone_number"
+              placeholder="Enter Phone Number"
+              label="Phone Number"
               size="large"
             />
           </Col>
-          <Col xs={24} md={12} lg={12} className="mt-3">
+          <Col xs={24} md={12} lg={6} className="mt-3">
+            <FormInput
+              name="other_phone_number"
+              placeholder="Enter Other Phone Number"
+              label="Phone Number"
+              size="large"
+            />
+          </Col>
+          <Col xs={24} md={12} lg={6} className="mt-3">
             <FormInput
               name="office_email"
               placeholder="Enter Office email"
@@ -83,153 +70,13 @@ const AdminBasicInfoForm = () => {
               size="large"
             />
           </Col>
-          <Col xs={24} md={12} lg={12} className="mt-3">
-            <NormalDatePicker
-              name="date_of_joining"
-              label="Date of Joining"
-              size="large"
-            />
-          </Col>
-        </Row>
-        <h3 className="text-lg mt-8 font-semibold underline">
-          Current Address
-        </h3>
-        <Row gutter={{ xs: 4, md: 20 }}>
-          <Col xs={24} md={12} lg={10} className="mt-3">
-            <FormInput
-              name="current_address.flat_number"
-              label="Flat Number"
-              placeholder="Enter Flat Number"
-              size="large"
-            />
-          </Col>
-          <Col xs={24} md={12} lg={14} className="mt-3">
-            <FormInput
-              name="current_address.building_name"
-              label="Building Name"
-              placeholder="Enter building number"
-              size="large"
-            />
-          </Col>
-          <Col xs={24} md={12} lg={24} className="mt-3">
-            <FormInput
-              name="current_address.street"
-              label="Address"
-              placeholder="Enter street name"
-              size="large"
-            />
-          </Col>
-          <Col xs={24} md={12} lg={24} className="mt-3">
-            <FormInput
-              name="current_address.landmark"
-              label="Landmark"
-              placeholder="Enter Landmark"
-              size="large"
-            />
-          </Col>
-          <Col xs={24} md={12} lg={18} className="mt-3">
-            <FormInput
-              name="current_address.city"
-              label="City"
-              placeholder="Enter city name"
-              size="large"
-            />
-          </Col>
           <Col xs={24} md={12} lg={6} className="mt-3">
             <FormInput
-              name="current_address.postal_code"
-              label="Post code"
-              placeholder="Enter post code"
+              name="personal_email"
+              placeholder="Enter Personal email"
+              label="Enter Personal Email"
+              type="email"
               size="large"
-              type="number"
-            />
-          </Col>
-          <Col xs={24} md={12} lg={12} className="mt-3">
-            <FormInput
-              name="current_address.state"
-              label="State"
-              placeholder="Enter state name"
-              size="large"
-            />
-          </Col>
-          <Col xs={24} md={12} lg={12} className="mt-3">
-            <FormSelectField
-              size="large"
-              name="current_address.country"
-              options={countryOptions}
-              label="Country"
-              placeholder="Select"
-            />
-          </Col>
-        </Row>
-        <h3 className="text-lg mt-8 font-semibold underline">
-          Permanent Address
-        </h3>
-        <Row gutter={{ xs: 4, md: 20 }}>
-          <Col xs={24} md={12} lg={10} className="mt-3">
-            <FormInput
-              name="permanent_address.flat_number"
-              label="Flat Number"
-              placeholder="Enter Flat Number"
-              size="large"
-            />
-          </Col>
-          <Col xs={24} md={12} lg={14} className="mt-3">
-            <FormInput
-              name="permanent_address.building_name"
-              label="Building Name"
-              placeholder="Enter building number"
-              size="large"
-            />
-          </Col>
-          <Col xs={24} md={12} lg={24} className="mt-3">
-            <FormInput
-              name="permanent_address.street"
-              label="Address"
-              placeholder="Enter street name"
-              size="large"
-            />
-          </Col>
-          <Col xs={24} md={12} lg={24} className="mt-3">
-            <FormInput
-              name="permanent_address.landmark"
-              label="Landmark"
-              placeholder="Enter Landmark"
-              size="large"
-            />
-          </Col>
-          <Col xs={24} md={12} lg={18} className="mt-3">
-            <FormInput
-              name="permanent_address.city"
-              label="City"
-              placeholder="Enter city name"
-              size="large"
-            />
-          </Col>
-          <Col xs={24} md={12} lg={6} className="mt-3">
-            <FormInput
-              name="permanent_address.postal_code"
-              label="Post code"
-              placeholder="Enter post code"
-              size="large"
-              type="number"
-            />
-          </Col>
-          <Col xs={24} md={12} lg={12} className="mt-3">
-            <FormInput
-              name="permanent_address.state"
-              label="State"
-              placeholder="Enter state name"
-              size="large"
-            />
-          </Col>
-          <Col xs={24} md={12} lg={12} className="mt-3">
-            <FormSelectField
-              size="large"
-              name="permanent_address.country"
-              options={countryOptions}
-              label="Country"
-              placeholder="Select"
             />
           </Col>
         </Row>

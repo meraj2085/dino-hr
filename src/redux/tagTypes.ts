ab508a31f638b96d5b21cfc6fbbeb160a8ac4fb2
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2024-01-01 20:40:16 +0600

    Merge conflict resolved

--- src/redux/tagTypes.ts
+++ src/redux/tagTypes.ts
@@ -13,16 +13,8 @@ export enum tagTypes {
   event = "event",
   notification = "notification",
   employee = "employee",
-<<<<<<< HEAD
-<<<<<<< HEAD
   leave = "leave",
-=======
-  attendance = "attendance"
->>>>>>> aeafeb6b99a829c451a5c7beff37c1266e4ad053
-=======
   attendance = "attendance",
-  leave = "leave",
->>>>>>> f5931bbc678f27265766c244722f18e7ea63c9d9
 }
 
 export const tagTypesList = [
@@ -40,14 +32,6 @@ export const tagTypesList = [
   tagTypes.event,
   tagTypes.notification,
   tagTypes.employee,
-<<<<<<< HEAD
-<<<<<<< HEAD
-  tagTypes.leave,
-=======
-  tagTypes.attendance,
->>>>>>> aeafeb6b99a829c451a5c7beff37c1266e4ad053
-=======
   tagTypes.attendance,
   tagTypes.leave,
->>>>>>> f5931bbc678f27265766c244722f18e7ea63c9d9
 ];

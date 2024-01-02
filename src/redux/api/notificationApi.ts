c349aac7314ca91f19274ed88dcf9ca8bc143ad7
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2024-01-02 15:26:21 +0600

    Adding notification

--- src/redux/api/notificationApi.ts
+++ src/redux/api/notificationApi.ts
@@ -31,8 +31,26 @@ export const notificationApi = baseApi.injectEndpoints({
       },
       providesTags: [tagTypes.notification],
     }),
+    getUnreadCount: build.query({
+      query: () => ({
+        url: `${NOTIFICATION_URL}/getUnreadCount`,
+        method: "GET",
+      }),
+      providesTags: [tagTypes.notification],
+    }),
+    deleteNotification: build.mutation({
+      query: () => ({
+        url: `${NOTIFICATION_URL}/`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [tagTypes.notification],
+    }),
   }),
 });
 
-export const { useAddNotificationMutation, useGetNotificationsQuery } =
-  notificationApi;
+export const {
+  useAddNotificationMutation,
+  useGetNotificationsQuery,
+  useGetUnreadCountQuery,
+  useDeleteNotificationMutation,
+} = notificationApi;

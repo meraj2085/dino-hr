811fddf4c98af0fa6039667b6d0565e6511f0f6b
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2024-01-02 16:14:38 +0600

    Updating profile page

--- src/app/(dashboardayout)/dashboard/profile/page.tsx
+++ src/app/(dashboardayout)/dashboard/profile/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import BreadCrumb from "@/components/ui/BreadCrumb";
 import {
   Avatar,
@@ -11,7 +12,6 @@ import {
   Timeline,
 } from "antd";
 import { EditOutlined } from "@ant-design/icons";
-import React from "react";
 
 const dataSource = [
   {
@@ -63,49 +63,48 @@ const profile = () => {
         ]}
       />
       {/* profile SECTION */}
-        <Card>
-          <Flex className="flex flex-col md:flex-row lg:gap-40">
-
-            {/* left  */}
-            <div className="flex">
-              {" "}
-              <Avatar
-                size={{ xs: 24, sm: 32, md: 40, lg: 80, xl: 100, xxl: 120 }}
-                src="https://smarthr.dreamstechnologies.com/html/template/assets/img/profiles/avatar-02.jpg"
-              />
-              <div className="pl-4">
-                <h1 className="text-xl font-bold">John Due</h1>
-                <h1 className="text-md font-bold">Web Developer</h1>
-                <h1 className="text-md font-bold">Employee ID:FT-0001</h1>
-                <Button className="my-2" type="primary" danger>
-                  Sent Message
-                </Button>
-              </div>
+      <Card>
+        <Flex className="flex flex-col md:flex-row lg:gap-40">
+          {/* left  */}
+          <div className="flex">
+            {" "}
+            <Avatar
+              size={{ xs: 24, sm: 32, md: 40, lg: 80, xl: 100, xxl: 120 }}
+              src="https://smarthr.dreamstechnologies.com/html/template/assets/img/profiles/avatar-02.jpg"
+            />
+            <div className="pl-4">
+              <h1 className="text-xl font-bold">John Due</h1>
+              <h1 className="text-md font-bold">Web Developer</h1>
+              <h1 className="text-md font-bold">Employee ID:FT-0001</h1>
+              <Button className="my-2" type="primary" danger>
+                Sent Message
+              </Button>
             </div>
+          </div>
 
-              {/* right */}
-              <div className="flex border-dashed lg:border-l-2 border-gray-500 pl-4">
-                {/* input */}
-                <div className="text-md font-bold mr-10">
-                  <h1>Phone:</h1>
-                  <h1>Email:</h1>
-                  <h1>Birthday:</h1>
-                  <h1>Address:</h1>
-                  <h1>Gender:</h1>
-                </div>
-                {/* value */}
-                <div>
-                  <h2 className="text-blue-500">3847234784</h2>
-                  <h2 className="text-blue-500">johndoe@example.com</h2>
-                  <h2>24th July</h2>
-                  <h2>1861 Bayonne Ave, NY, US</h2>
-                  <h2>Male</h2>
-                </div>
-              </div>
-          </Flex>
-        </Card>
-        {/* </Col> */}
- 
+          {/* right */}
+          <div className="flex border-dashed lg:border-l-2 border-gray-500 pl-4">
+            {/* input */}
+            <div className="text-md font-bold mr-10">
+              <h1>Phone:</h1>
+              <h1>Email:</h1>
+              <h1>Birthday:</h1>
+              <h1>Address:</h1>
+              <h1>Gender:</h1>
+            </div>
+            {/* value */}
+            <div>
+              <h2 className="text-blue-500">3847234784</h2>
+              <h2 className="text-blue-500">johndoe@example.com</h2>
+              <h2>24th July</h2>
+              <h2>1861 Bayonne Ave, NY, US</h2>
+              <h2>Male</h2>
+            </div>
+          </div>
+        </Flex>
+      </Card>
+      {/* </Col> */}
+
       {/* 2nd section  */}
       <br />
 

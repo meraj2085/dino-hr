538cde35237c1838661af9e4bbe33fb2d5b5e5ed
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2024-01-03 21:49:45 +0600

    Fixed fetch error

--- src/app/(homelayout)/layout.tsx
+++ src/app/(homelayout)/layout.tsx
@@ -1,20 +1,7 @@
 "use client";
-
-import dynamic from "next/dynamic";
 import NavBar from "@/components/shared/Navbar";
-import { useGetProfileQuery } from "@/redux/api/userApi";
-import { getUserInfo } from "@/services/auth.service";
-import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import { useEffect } from "react";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  const dispatch = useAppDispatch();
-  const { data, isLoading } = useGetProfileQuery(undefined);
-
-  useEffect(() => {
-    dispatch({ type: "user/setUser", payload: data });
-  }, [data, dispatch]);
-
   return (
     <div>
       <NavBar />

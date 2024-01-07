fed2425a3b38e9dd1ccf11c7510f3c3f092abf92
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2024-01-07 14:45:19 +0600

    Updated our tea,

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -71,10 +71,14 @@ const OurTeam = () => {
               Meet our team of experts who are always ready to help you.
             </p>
           </div>
-          <div className="flex flex-wrap -m-2">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
             {TeamMemberDetails.map((member, index) => (
-              <div key={index} className="p-2 lg:w-1/3 md:w-1/2 w-full group">
-                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg hover:shadow-md transition duration-300 ease-in-outd">
+              <div
+                key={index}
+                className="w-full group overflow-hidden relative hover:bg-gradient-to-r transition-all ease-out duration-300"
+              >
+                <span className="absolute right-0 w-8 h-32 -mt-1 transition-all duration-1000 transform translate-x-12 bg-[#4fad92] opacity-10 rotate-12 group-hover:-translate-x-[380px] ease"></span>
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg transition duration-300 ease-in-outd">
                   <Image
                     className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
                     alt="team"
